5629fafeda81a75846d6460ebc894165c4c82dd4
LaurAndreea10 <79794410+LaurAndreea10@users.noreply.github.com>
2026-09-16 08:47:40 +0300

    chore: refresh PWA cache for free mode selection

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tri-link-quest-v1';
+const CACHE='tri-link-quest-v2';
 const CORE=['./','./index.html','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png','./assets/tri-link-quest-og.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
